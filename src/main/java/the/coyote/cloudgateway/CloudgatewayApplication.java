6dc376abf3313cd4fec15079637492c36582cde0
theCoyote <flaviolunaferreira@gmail.com>
2024-08-30 17:55:52 -0300

    deploy inicial

---
+++ src/main/java/the/coyote/cloudgateway/CloudgatewayApplication.java
@@ -0,0 +1,26 @@
+package the.coyote.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudgatewayApplication.class, args);
+	}
+	
+	@Bean
+    RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+		    .routes()
+			    .route( r -> r.path("/clientes/**").uri("lb://clientes"))
+			.build();
+	}
+
+}
